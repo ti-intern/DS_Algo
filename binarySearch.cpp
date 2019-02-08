895b22697292d26b565c1d7a7c55399105ac243b
ti-intern <rahul.tiintern@gmail.com>
2019-02-08 19:12:36 +0530

    Basic Searching Codes

---
+++ binarySearch.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+int find(int *a,int key,int size)
+{
+	int start = 0;
+	int end = size-1;
+	int mid;
+
+	while(start<=end){
+		mid = (start+end)/2;
+		if(a[mid]==key)
+			return mid;
+		else if(a[mid]>key)
+			end = mid-1;
+		else
+			start = mid+1;
+	}
+
+	return -1;
+}
+
+int main()
+{
+	int a[] = {1,2,3,4,5,6,7};
+	int key = 3;
+	int size = sizeof(a)/sizeof(int);
+	int ans = find(a,key,size);
+	cout<<ans<<endl;
+	return 0;
+}
---
+++ linearSearch.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int find(int *a,int key,int size)
+{
+	for(int i=0;i<size;i++){
+		if(a[i]==key)
+			return i;
+	}
+	return -1;
+}
+
+int main()
+{
+	int a[] = {1,2,3,4,5,6,7};
+	int key = 3;
+	int size = sizeof(a)/sizeof(int);
+	int ans = find(a,key,size);
+	cout<<ans<<endl;
+	return 0;
+}
